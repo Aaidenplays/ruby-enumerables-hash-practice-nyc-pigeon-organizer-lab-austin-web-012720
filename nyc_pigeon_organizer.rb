0a98a57e8df72504e424739162987a86b1d783cb
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-09 23:53:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,16 @@ def set_name_keys (data)
   
 end
 
-def 
+def set_name_colors(data)
+  
+end
 
+def set_name_lives(data)
+  
+end
+
+def set_name_gender(data)
+  
 end
 
 def nyc_pigeon_organizer(data)
