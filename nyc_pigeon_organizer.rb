81d3af31008cf290b1825aacfffb2435554681a9
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 00:05:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require 'pry'
 def set_name_keys (data)
   new_hash = {}
   tester = []
-  data.each { |n| n.each{|i| new_hash.include?(i)}}
+  new hash << data.each { |n| n.each{|i| new_hash.include?(i)}}
   binding.pry
 end
 
