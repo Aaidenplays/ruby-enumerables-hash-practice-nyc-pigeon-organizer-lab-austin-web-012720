26af8295b89589f70c538a6a698d68b78347e8dc
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 01:34:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -33,7 +33,7 @@ def set_values(data, new_hash)
     data_hash.each do |attributes, array|
       array.each do |name|
         if !array.include?(new_hash[name])
-          new_hash[name][first_keys] << attributes
+          new_hash[name][first_keys] << "#{attributes}"
         end
                  end
                    end
@@ -48,7 +48,10 @@ def nyc_pigeon_organizer(data)
   colored_hash = set_attributes_keys(data, named_hash)
   final_hash = set_values(data, colored_hash)
   binding.pry
-
+  
+  final_hash.each do |name_hash, attributes|
+          
+                  end
 
   final_hash
 end
