52c18dc50b766c1a5efaf8b3bc29be24ace806f9
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 00:49:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,16 @@
 require 'pry'
 
 def set_name_keys (data)
-
-
+  new_hash = {}
+  data.each do |first_keys, data_hash|
+    data_hash.each do |attributes, array|
+        array.each do |name|
+          if !new_hash.has_key?(name)
+            new_hash[name] = {}
+          end
+        end
+      end
+    end
 end
 
 def set_name_colors(data)
@@ -18,18 +26,9 @@ def set_name_gender(data)
 end
 
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |first_keys, data_hash|
-    data_hash.each do |attributes, array|
-        array.each do |name|
-          if !new_hash.has_key?(name)
-            new_hash[name] = {}
-          end
-        end
-      end
-    end
 
 
-  set_name_keys(data)
+
+  new_hash = set_name_keys(data)
   new_hash
 end
