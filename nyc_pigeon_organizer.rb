86fcbc9de896fea0d3a02235d7105f7bf0667171
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 01:11:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,7 +24,6 @@ def set_attributes_keys(data, new_hash)
                                 end
                       end
             end
-binding.pry
 new_hash
 end
 
@@ -38,6 +37,7 @@ def nyc_pigeon_organizer(data)
   named_hash = set_name_keys(data)
   colored_hash = set_attributes_keys(data, named_hash)
   final_hash = set_values(data, colored_hash)
+binding.pry
 
 
   new_hash
